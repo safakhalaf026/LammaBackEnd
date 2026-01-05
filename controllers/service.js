5fc737afa1bdc1257ffff76d3f3fecfdb8edea11
safakhalaf026 <safakhalaf026@gmail.com>
2026-01-05 20:00:34 +0300

    added /services POST route

--- controllers/service.js
+++ controllers/service.js
@@ -4,14 +4,27 @@ const router = express.Router()
 const User = require('../models/User')
 const Service = require('../models/Service')
 
-router.post('/', async (req, res) =>{
+router.post('/', async (req, res) => {
     try {
-        const userId = req.user.
-        
+        // role validation
+        if(req.user.role !== 'Service Provider'){
+            return res.status(403).json({ err: 'Only service providers can create services' })
+        }
+
+        // reference the logged in service provider 
+        const serviceData = {
+            ...req.body,
+            provider: req.user._id,
+        }
+
+        // create record
+        const service = await Service.create(serviceData)
+
+        res.status(201).json({service})
     } catch (err) {
         console.error(err)
-        res.status(500).json({err:'Internal Server Error'})
+        res.status(500).json({ err: 'Failed to create service' })
     }
 })
 
-module.exports=router
+module.exports = router
